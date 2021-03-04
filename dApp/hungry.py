096d31111d9d0d879c394ff0f092053c34274b65
choudharirohit1995 <rc5741545@gmail.com>
2021-03-04 18:53:03 +0530

    added hungry prompt

--- dApp/hungry.py
+++ dApp/hungry.py
@@ -1,4 +1,6 @@
-a = int(input('Enter a 1 no:'))
-b = int(input('Enter a 2 no:'))
+a = float(input('Enter a 1 no:'))
+b = float(input('Enter a 2 no:'))
+a  = int(a)
+b = int(b)
 add  = a+b
 print('The addition i:',add)
